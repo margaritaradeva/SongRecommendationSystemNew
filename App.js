3da207b9fc84290452542d541b03feff6cf6a15a
margaritaradeva <u12mr20@abdn.ac.uk>
2024-12-13 00:57:37 +0000

    changed frontend to expo

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer} from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Home from './Home';
+import SongRecommendation from './SongRecommendation';
+import { Platform } from 'react-native';
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = 'home';
+            } else if (route.name === 'Recommend') {
+              iconName = 'music';
+            }
+            // You can return any component that you like here!
+            return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'yellowgreen',
+          tabBarInactiveTintColor: 'gray',
+          tabBarStyle: {
+            ...Platform.select({
+              web: {
+                backgroundImage: 'linear-gradient(to right,#a5a0fc,#7ae3f8,#00d4ff)',
+                color: 'white',
+                height: 60,
+              },
+              default: {
+                backgroundColor: 'green',
+                height: 60,
+              },
+            })
+          },
+          
+          
+          headerShown: false, // Hide the header
+        })}
+      >
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Recommend" component={SongRecommendation} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ Home.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { StyleSheet, Platform, Text, View } from 'react-native';
+import LottieView from 'lottie-react-native';
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.sound}>
+        <LottieView
+          source={require('./assets/lottie/sound.json')} // Ensure the path is correct
+          autoPlay
+          loop
+          style={styles.lottie}
+        />
+      </View>
+      <Text style={styles.title}>Song Recommendation System</Text>
+      <Text style={styles.subtitle}>Group Alpha</Text>
+      <Text style={styles.description}>
+        Welcome to our Song Recommendation Engine. This tool recommends songs based on your
+        listening preferences, helping you discover music that resonates with your taste.
+      </Text>
+      <View style={styles.lottieContainer}>
+        <LottieView
+          source={require('./assets/lottie/littleMan.json')} // Ensure the path is correct
+          autoPlay
+          loop
+          style={styles.lottie}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: Platform.OS === 'web' ? '100%' : '100%',
+    backgroundImage: Platform.OS === 'web' ? 'linear-gradient(to right top, #26cebd, #0dd9a8, #41e288, #74e85c, #a8eb12)' : undefined,
+    padding: 20,
+  },
+  title: {
+    fontSize: Platform.OS === 'web' ? 54 : 24,
+    fontWeight: 'bold',
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  subtitle: {
+    fontSize: 28,
+    fontStyle: 'italic',
+    marginBottom: 16,
+    textAlign: 'center',
+    color: '#333',
+    width: '100%',
+  },
+  description: {
+    fontSize: 16,
+    textAlign: 'center',
+    paddingHorizontal: 30,
+    lineHeight: 24,
+    borderWidth: 1,
+    borderColor: '#000',
+    padding: 10,
+    marginTop: 20,
+    borderRadius: 5,
+    width: '100%',
+  },
+  lottieContainer: {
+    width: 600, // Set width of the container
+    height: 600, // Set height of the container
+    justifyContent: 'center', // Center the LottieView vertically
+    alignItems: 'center', // Center the LottieView horizontally
+  },
+  lottie: {
+    width: 150, // Set the width of the LottieView
+    height: 150, // Set the height of the LottieView
+  },
+  sound: {
+    width: 150,
+    height: 150,
+  },
+});
